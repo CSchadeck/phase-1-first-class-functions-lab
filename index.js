29e9975f2a5bae2867862245291e2e7cde0f7f19
Corey Schadeck <corey@schadeck.com>
2022-04-27 22:05:47 -0400

    commit tree a7c64280a27561027ccafadd1b7d7614d7b52fde

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Code your solution in this file!
+
+const returnFirstTwoDrivers = function (driverGroup) {
+    return [driverGroup [0], driverGroup [1]];
+};
+
+const returnLastTwoDrivers = function (driverGroup) {
+        return driverGroup.slice (-2);
+};
+
+const selectingDrivers = [
+    returnFirstTwoDrivers,
+    returnLastTwoDrivers
+];
+
+
+const createFareMultiplier = function (multiplier) {
+    const makeMultiplier = function(fare) {
+        return multiplier * fare;
+    }
+    return makeMultiplier;
+};
+
+// need help here
+const fareDoubler = createFareMultiplier(2)
+
+// need help here
+const fareTripler = function (fare) {
+    const createFareMultiplier =  function (fare)  {
+        return fare * 3;
+    };
+    return createFareMultiplier;
+};
+
+const selectDifferentDrivers = function (driverArray, driverFunction) {
+    return driverFunction(driverArray)
+};
